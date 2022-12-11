5b8917ef0b7e2e0b3cd1a15a60db47b6b9916752
winir <44081810+winir@users.noreply.github.com>
2022-12-11 19:25:04 +1100

    Update start.js

--- start.js
+++ start.js
@@ -4,24 +4,7 @@
 $(document).ready(function() {
 
 	console.log('start ready')
+	alert("Ready);
 
-  util.init();
-
-	$(function(){	            
-		  $("#main_menu").load("./templates/main_menu.html"); 
-      $("#content").load("./templates/home.html"); 
-      $("#footer").html(util.footer());
-	  });
-});
-
-//navbar handler
-$('.nav').on('click','a', function(e){    
-  var id = $(this).attr("id");
-    console.log(id); 
-  if (id==="1") {$("#content").load("./templates/home.html");  }; //home
-  if (id==="2") {$("#content").load("./templates/products.html");  }; //products
-  if (id==="3") {$("#content").load("./templates/members.html");  }; //members
-  if (id==="4") {$("#content").load("./templates/about.html");  }; //about
-  if (id==="5") {$("#content").load("./templates/contact.html");  }; //contact
-  if (id==="6") {$("#content").load("./templates/faq.html");  }; //FAQ       
+  
 });//end function
