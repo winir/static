14e8fcbbb7f2fa37f3cfb4d3d3a3c1e1d81f266f
winir <44081810+winir@users.noreply.github.com>
2022-12-11 19:42:40 +1100

    Add files via upload

--- start.js
+++ start.js
@@ -1,10 +1,25 @@
-/** start.js */
+//start.js
+
+/* url of the api.php*/
+url_api="api.php";
+
+//****** logging ****/
+
+var _data=[]; // hold returned data from DB
+var _user=''; /// logged in user
+var _logged_user={};
+var _timestamp='';
+//****** logging ****/
+
+
 
 //jquery ready
 $(document).ready(function() {
 
 	console.log('start ready')
-	alert("Ready");
 
-  
-});//end function
+	$(function(){		
+		$("#frame").load("templates/blog_form.html"); 
+	  });
+});
+
